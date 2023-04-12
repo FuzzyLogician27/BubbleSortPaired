1d84cb48528230f460b245797dbf8c3b62e2e8d1
coffeeandcodee <a9idris@gmail.com>
2023-04-12 15:36:25 +0100

    Phase 1 complete

--- BubbleSortApp/BubbleSortApp/Program.cs
+++ BubbleSortApp/BubbleSortApp/Program.cs
@@ -4,7 +4,45 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] testArr = new int[3] { 3, 2, 1 };
+            BubbleSort(ref testArr);
+
+            //NICECCCCCECECEC 
+
+            foreach(int n in testArr)
+            {
+                Console.WriteLine(n);
+            }
+            
+        }
+
+        public static void BubbleSort(ref int[] arr)
+        {
+            int temp = 0;
+            bool clean = false;
+
+            while (clean == false)
+            {
+
+                //Lowest to highest BUBBLING
+                clean = true;
+                for (int i = 0; i < arr.Length - 1; i++)
+                {
+                    temp = arr[i];
+                    if (arr[i] > arr[i + 1])
+                    {
+                        arr[i] = arr[i + 1];
+                        arr[i+1] = temp;
+                        clean = false;
+                    }
+                        
+                        
+                }
+
+            }
+
+            
+
         }
     }
 }
